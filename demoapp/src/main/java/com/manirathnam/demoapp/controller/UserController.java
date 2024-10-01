023e7bc4ff17bb5615044f8dc2b3dfbfb9afdee9
manirathnam5 <manirathnam501@gmail.com>
2024-10-01 22:37:38 +0530

    usercontroller .java

--- demoapp/src/main/java/com/manirathnam/demoapp/controller/UserController.java
+++ demoapp/src/main/java/com/manirathnam/demoapp/controller/UserController.java
@@ -11,31 +11,26 @@ public class UserController {
     public String getUser(){
         return "USERS FETCHED";
     }
-
-
+    
     @GetMapping("/{userId}")
     public String getUsers(@PathVariable String userId){
         return "HELLO USERS  " + userId ;
     }
-
 
     @GetMapping
     public String getSetOfUsers(@RequestParam(value="page") String pageNo,@RequestParam(value="limit") String pageSize){
         return "HELLO USERS from  " + pageNo + " of page size " + pageSize ;
     }
 
-
     @PostMapping("/save")
     public String saveUser(){
         return "USERS SAVED";
     }
 
-
     @PutMapping("/update")
     public String updatetUser(){
         return "USERS UPDATED";
     }
-
 
     @DeleteMapping("/delete")
     public String deleteUser(){
